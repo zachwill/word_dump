d9c41646e89f438423563d933e45f4b287efdb20
Zach Williams <hey@zachwill.com>
2012-05-17 21:12:32 -0700

    Add random words with JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(function(){
-  var words = _.shuffle(window._words);
+  var words = _.shuffle(window._words),
+      length = words.length;
+
+  var word = function(){
+    var number = Math.floor(Math.random() * length);
+    return words[number];
+  };
 
   var create = function(e){
-    var random = [words.pop(), words.pop()].join(' ');
+    var random = [word(), word()].join(' ');
     $('.words').html(random);
     return false;
-  }
+  };
 
   create();
 
